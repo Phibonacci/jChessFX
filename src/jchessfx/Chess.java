0fb35837bdb9727d9dd4ad66a7f3f31cf60a01a4
leduc_p <leduc_p@epitech.eu>
2015-11-23 16:31:51 +0000

    Add empty JavaFX eclipse project

---
+++ src/jchessfx/Chess.java
@@ -0,0 +1,16 @@
+package jchessfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chess extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("jChessFX");
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
