cae605a8a4b1e25e49cc1e7b2f494244777a0760
Phibonacci <jean.fauquenot@gmail.com>
2015-11-24 12:52:49 +0000

    Add Square class to draw the board background

---
+++ src/jchessfx/Square.java
@@ -0,0 +1,29 @@
+package jchessfx;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Square extends Group {
+	private Rectangle shape;
+	
+	public Square(int team) {
+		// make a new Ellipse and Translate, add the Translate to the Ellipse, add the Ellipse to the Group 
+		shape = new Rectangle();
+		getChildren().addAll(shape);
+		if (team == Piece.WHITE) {
+			shape.setStroke(Color.WHITE);
+			shape.setFill(Color.WHITE);
+		} else if (team == Piece.BLACK) {
+			shape.setStroke(Color.BLACK);
+			shape.setFill(Color.BLACK);
+		}
+	}
+	
+	@Override
+	public void resize(double width, double height) {
+		super.resize(width, height);
+		shape.setWidth(width);
+		shape.setHeight(height);
+	}
+}
