9c26ce89f50304e67be43d7359cadfab8e0d5568
Phibonacci <jean.fauquenot@gmail.com>
2015-11-25 21:14:37 +0000

    Fix pieces positions

--- src/jchessfx/ChessBoard.java
+++ src/jchessfx/ChessBoard.java
@@ -45,6 +45,10 @@ public class ChessBoard extends Pane {
 			for(int j = 0; j < board[i].length; j++) {
 				squares[i][j].resize(cellWidth, cellHeight);
 				squares[i][j].relocate(i * cellWidth, j * cellHeight);
+				if (board[i][j] != null) {
+					board[i][j].resize(cellWidth, cellHeight);
+					board[i][j].relocate(j * cellWidth, i * cellHeight);
+				}
 			}
 		}
 	}
@@ -52,25 +56,44 @@ public class ChessBoard extends Pane {
 	public void resetGame() {
 		currentPlayer = Piece.WHITE;
 		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if (board[i][j] != null) {
+					getChildren().remove(board[i][j]);
+					board[i][j] = null;
+				}
+			}
+		}
+		for(int i = 0; i < board.length; i++) {
 			if (i == 0 || i == 7) {
 				int team = (i == 0 ? Piece.WHITE  : Piece.BLACK);
-				board[i][0] = new PieceKnight(team);
-				board[i][7] = new PieceKnight(team);
-				board[i][1] = new PieceRook  (team);
-				board[i][6] = new PieceRook  (team);
+				board[i][0] = new PieceRook  (team);
+				getChildren().add(board[i][0]);
+				board[i][7] = new PieceRook  (team);
+				getChildren().add(board[i][7]);
+				board[i][1] = new PieceKnight(team);
+				getChildren().add(board[i][1]);
+				board[i][6] = new PieceKnight(team);
+				getChildren().add(board[i][6]);
 				board[i][2] = new PieceBishop(team);
+				getChildren().add(board[i][2]);
 				board[i][5] = new PieceBishop(team);
-				board[i][3] = new PieceQueen (team);
-				board[i][4] = new PieceKing  (team);
+				getChildren().add(board[i][5]);
+				board[i][3] = new PieceKing  (team);
+				getChildren().add(board[i][3]);
+				board[i][4] = new PieceQueen (team);
+				getChildren().add(board[i][4]);
 			}
-			for(int j = 0; j < board[i].length; j++) {
-				getChildren().remove(board[i][j]);
-				if (i == 1) {
-					board[i][j] = new PiecePawn(Piece.WHITE);
-				} else if (i == 6) {
-					board[i][j] = new PiecePawn(Piece.BLACK);
-				} else {
-					board[i][j] = null;
+			else {
+				for(int j = 0; j < board[i].length; j++) {
+					if (i == 1) {
+						board[i][j] = new PiecePawn(Piece.WHITE);
+						getChildren().add(board[i][j]);
+					} else if (i == 6) {
+						board[i][j] = new PiecePawn(Piece.BLACK);
+						getChildren().add(board[i][j]);
+					} else {
+						board[i][j] = null;
+					}
 				}
 			}
 		}
